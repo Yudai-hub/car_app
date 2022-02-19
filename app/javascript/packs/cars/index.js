dde11c4baa658e408ea671c7f175c243fa25fa66
Yudai-hub <get.on.well0717@gmail.com>
2022-02-19 12:25:22 +0900

    js: google-search-api -> put image

--- app/javascript/packs/cars/index.js
+++ app/javascript/packs/cars/index.js
@@ -9,18 +9,33 @@ function loadDoc() {
 
   //mandatory code: here
   const xhttp_car = new XMLHttpRequest();
+  const xhttp_car_image = new XMLHttpRequest();
 
   // after required
-  xhttp_car.onreadystatechange = function () {
-    document.getElementById("car").innerHTML = this.status + this.readyState;
+  // xhttp_car.onreadystatechange = function () {
+  //   document.getElementById("car").innerHTML = this.status + this.readyState;
+  //   if (this.readyState == 4 && this.status == 200) {
+  //     const obj = JSON.parse(this.response)
+  //     var cars = obj.data
+  //     let cars_name = [];
+  //     for (let car of cars) {
+  //       cars_name.push(car.maker + " " + car.model + " " + car.make_year);
+  //     }
+  //     document.getElementById("car").innerHTML = cars_name;
+  //   }
+  // };
+  xhttp_car_image.onreadystatechange = function () {
+    // document.getElementById("car").innerHTML = this.status + this.readyState + this.responseText;
     if (this.readyState == 4 && this.status == 200) {
       const obj = JSON.parse(this.response)
-      var cars = obj.data
-      let cars_name = [];
-      for (let car of cars) {
-        cars_name.push(car.maker + " " + car.model + " " + car.make_year);
+      var search_results = obj.items
+      let car_image_urls = [];
+      for (let result of search_results) {
+        car_image_urls.push(result.link);
       }
-      document.getElementById("car").innerHTML = cars_name;
+      // document.getElementById("car").innerHTML = car_image_urls;
+      document.getElementById("image").style = "height:600px;"
+      document.getElementById("image").src = car_image_urls[0];
     }
   };
 
@@ -33,7 +48,9 @@ function loadDoc() {
 
   // navigator.geolocation.getCurrentPosition(success);
   function success(position) {
-    xhttp_car.open("GET", `https://my-api--yudai-hub004.herokuapp.com/api/v1/cars`);
-    xhttp_car.send();
+    // xhttp_car.open("GET", `https://my-api--yudai-hub004.herokuapp.com/api/v1/cars`);
+    // xhttp_car.send();
+    xhttp_car_image.open("GET", `https://www.googleapis.com/customsearch/v1?&key=AIzaSyBAulCi8Iz_bsZTF5Yt0B_S3uydgUewL1w&cx=3beab832c371c3ee9&q=toyota+camry&searchType=IMAGE`);
+    xhttp_car_image.send();
   }
 }

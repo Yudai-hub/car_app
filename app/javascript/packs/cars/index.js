87e0fa9890ea2f7348a171ba1595397a4abdca11
Yudai-hub <get.on.well0717@gmail.com>
2022-02-20 16:56:42 +0900

    put img from api

--- app/javascript/packs/cars/index.js
+++ app/javascript/packs/cars/index.js
@@ -11,19 +11,75 @@ function loadDoc() {
   const xhttp_car = new XMLHttpRequest();
   const xhttp_car_image = new XMLHttpRequest();
 
+
+  //処理フラグ（true:処理済、false:処理中）
+  var isProcessEnd;
+
+  //メイン処理
+  isProcessEnd = false;
+
+  //1つ目のajax処理
+  ajaxFunc1();
+  //2つ目のajax処理を実行するためのタイマー処理
+  timerFunc();
+
+  //タイマー処理
+  function timerFunc() {
+    if (isProcessEnd == false) {
+      //1つ目のajax処理が処理中なら1秒後に自関数を呼び出す
+      setTimeout(
+        function () {
+          timerFunc();
+        }, 1000
+      )
+    } else {
+      //フラグが処理済みになっていれば、2つ目のajax処理を実行する
+      ajaxFunc2();
+    }
+  }
+
+  //1つ目のajax処理
+  function ajaxFunc1() {
+    try {
+      xhttp_car.open("GET", `https://my-api--yudai-hub004.herokuapp.com/api/v1/cars`);
+      xhttp_car.send();
+    } catch {
+      car.innerHTML = err;
+    }
+  }
+
+  var car_names = [];
+
   // after required
-  // xhttp_car.onreadystatechange = function () {
-  //   document.getElementById("car").innerHTML = this.status + this.readyState;
-  //   if (this.readyState == 4 && this.status == 200) {
-  //     const obj = JSON.parse(this.response)
-  //     var cars = obj.data
-  //     let cars_name = [];
-  //     for (let car of cars) {
-  //       cars_name.push(car.maker + " " + car.model + " " + car.make_year);
-  //     }
-  //     document.getElementById("car").innerHTML = cars_name;
-  //   }
-  // };
+  xhttp_car.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const obj = JSON.parse(this.response)
+      var cars = obj.data
+      for (let car of cars) {
+        car_names.push(`${car.maker} ${car.model} ${car.make_year}`);
+      }
+    }
+    document.getElementById("car").innerHTML = this.status + this.readyState + car_names[0];
+    //フラグを処理済みにする
+    isProcessEnd = true;
+  };
+
+  //2つ目のajax処理
+  function ajaxFunc2() {
+    try {
+      let searchParams = new URLSearchParams({ q: car_names[0] });
+      let search_params = searchParams.toString();
+      xhttp_car_image.open("GET", `https://www.googleapis.com/customsearch/v1?&key=AIzaSyBAulCi8Iz_bsZTF5Yt0B_S3uydgUewL1w&cx=3beab832c371c3ee9&${search_params}&searchType=IMAGE`);
+      xhttp_car_image.send();
+    } catch {
+      car.innerHTML = err;
+    }
+
+    //フラグを初期化
+    isProcessEnd = false;
+  }
+
+
   xhttp_car_image.onreadystatechange = function () {
     // document.getElementById("car").innerHTML = this.status + this.readyState + this.responseText;
     if (this.readyState == 4 && this.status == 200) {
@@ -39,18 +95,5 @@ function loadDoc() {
     }
   };
 
-  navigator.geolocation.getCurrentPosition(success)
-  try {
-    navigator.geolocation.getCurrentPosition(success);
-  } catch {
-    car.innerHTML = err;
-  }
 
-  // navigator.geolocation.getCurrentPosition(success);
-  function success(position) {
-    // xhttp_car.open("GET", `https://my-api--yudai-hub004.herokuapp.com/api/v1/cars`);
-    // xhttp_car.send();
-    xhttp_car_image.open("GET", `https://www.googleapis.com/customsearch/v1?&key=AIzaSyBAulCi8Iz_bsZTF5Yt0B_S3uydgUewL1w&cx=3beab832c371c3ee9&q=toyota+camry&searchType=IMAGE`);
-    xhttp_car_image.send();
-  }
 }
